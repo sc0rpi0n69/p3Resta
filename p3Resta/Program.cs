8dee4cd8bff623565c0eba83a686d27b34df5839
Jesus <sc0rpi0n69@hotmail.com>
2015-09-13 17:32:26 -0500

    Mostrar el resultado

--- p3Resta/Program.cs
+++ p3Resta/Program.cs
@@ -15,6 +15,11 @@ namespace p3Resta
 
             Console.WriteLine("Dame el segundo numero: ");
             int numero2 = int.Parse(Console.ReadLine());
+
+            int resultado = numero1 - numero2;
+            Console.WriteLine("El resultado es: " + resultado);
+
+            Console.ReadLine();
         }
     }
 }
