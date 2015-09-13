0dc7f0db78e9b2511aadf1877bd6b7240ea422c4
Jesus <sc0rpi0n69@hotmail.com>
2015-09-13 16:58:56 -0500

    base del proyecto

---
+++ p3Resta/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p3Resta
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            Console.Write("Por favor precione cualquier tecla para continuar");
+            Console.ReadKey(true);
+        }
+    }
+}
